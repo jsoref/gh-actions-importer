4694ca63000963f25a1d704a2bfd880db25ece8a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-12-21 10:08:09 +0930

    Remove duplicated word `and`

--- src/ActionsImporter/Commands/Travis/Migrate.cs
+++ src/ActionsImporter/Commands/Travis/Migrate.cs
@@ -10,7 +10,7 @@ public class Migrate : ContainerCommand
     }
 
     protected override string Name => "travis-ci";
-    protected override string Description => "Convert a Travis CI pipeline to a GitHub Actions workflow and and open a pull request with the changes.";
+    protected override string Description => "Convert a Travis CI pipeline to a GitHub Actions workflow and open a pull request with the changes.";
 
     protected override ImmutableArray<Option> Options => ImmutableArray.Create<Option>(
         Common.Repository,
